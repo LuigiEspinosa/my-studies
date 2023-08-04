a33b4e59fc6efea663392522639417e31dafe486
LuigiEspinosa <numerocuatro@pm.me>
2023-08-04 12:34:20 -0500

    Vue.js 3 Crash Course for beginners - insidewebdev Youtube Channel

---
+++ Youtube/insidewebdev/Vue 3 Crash Course/vue-project/src/components/Form.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  inputValue: String,
+  required: true,
+});
+
+const emit = defineEmits(['submit']);
+</script>
+
+<template>
+  <h1>FORM</h1>
+
+  <form @submit.prevent="(e) => emit('submit', e)">
+    <input type="text" :value="props.inputValue" />
+    <input type="submit" />
+  </form>
+</template>
