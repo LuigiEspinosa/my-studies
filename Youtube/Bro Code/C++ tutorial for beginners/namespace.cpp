fe4f4a1dcbf0ccbbd6551cb85da48838a4ca2ce9
LuigiEspinosa <numerocuatro@pm.me>
2023-09-15 22:46:21 -0500

    C++ output, comments, and escape sequences (#2) by Bro Code

---
+++ Youtube/Bro Code/C++ tutorial for beginners/namespace.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // This is an inline comment in C++
+
+  /*
+   * This is a Comment Block
+   * I can add as much as I want
+   */
+
+  cout << "Cout using namespace" << endl;
+
+  return 0;
+}
